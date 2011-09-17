fde500dfd91972c6295298448a36176dff5e98fb
Ming-Ho Yee <ming-ho.yee@uwaterloo.ca>
2011-09-17 17:48:10 -0400

    Basic signup page for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Wishlist::Application.routes.draw do
   root :to => "pages#home"
 
-  match "/about", :to => "pages#about"
-  match "/help",  :to => "pages#help"
+  match "/about",   :to => "pages#about"
+  match "/help",    :to => "pages#help"
+  match "/signup",  :to => "users#new"
+
+  get "users/new"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
