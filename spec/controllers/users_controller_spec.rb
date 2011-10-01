381f115eabb666868b07ecc12daca00a39f0d6fd
Ming-Ho Yee <ming-ho.yee@uwaterloo.ca>
2011-10-01 15:26:49 -0400

    Tweaks to functionality. Added fixtures and specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe UsersController do
   render_views
+  fixtures :users
 
   describe "GET 'new'" do
     it "should be successful" do
@@ -16,9 +17,58 @@ describe UsersController do
   end
 
   describe "GET 'show'" do
+    before(:each) do
+      @user = User.first
+    end
+
     it "should be successful" do
-      get "show", :id => 1
+      get "show", :id => @user
       response.should be_success
+      assigns(:user).should == @user
+    end
+
+    it "should have the right title" do
+      get "show", :id => @user
+      response.should have_selector("title", :content => @user.name)
+    end
+
+    it "should include the user's name" do
+      get "show", :id => @user
+      response.should have_selector("h1", :content => @user.name)
+    end
+  end
+
+  describe "POST 'create'" do
+    it "should redirect on successful registration" do
+      params = {
+        "name" => "Johnny H. Smith",
+        "username" => "jhsmith1",
+        "password" => "password",
+        "password_confirmation" => "password"
+      }
+      user = mock_model(User, params)
+
+      User.should_receive(:new).with(params).and_return(user)
+      user.should_receive(:save).and_return(true)
+
+      post "create", :user => params
+      response.should be_redirect
+    end
+
+    it "should not redirect on unsuccessful registration" do
+      params = {
+        "name" => "Johnny H. Smith",
+        "username" => "jhsmith",
+        "password" => "password",
+        "password_confirmation" => "password"
+      }
+      user = mock_model(User, params)
+
+      User.should_receive(:new).with(params).and_return(user)
+      user.should_receive(:save).and_return(false)
+
+      post "create", :user => params
+      response.should_not be_redirect
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  ENV["RAILS_ENV"] ||= 'test'
+  ENV["RAILS_ENV"] = 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
 
@@ -14,9 +14,7 @@ Spork.prefork do
   RSpec.configure do |config|
     # Mock Framework
     config.mock_with :rspec
-
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
     config.use_transactional_fixtures = true
   end
 end
