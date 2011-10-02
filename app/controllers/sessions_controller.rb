b11af04c8b4d274ad85deb9a2063461d517c6cba
Ming-Ho Yee <ming-ho.yee@uwaterloo.ca>
2011-10-02 14:27:19 -0400

    Refactor sessions form. Use DELETE (instead of POST) to log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+
   def new
     @title = "Log in"
   end
 
   def create
-    user = User.find_by_username(params[:username])
-    if user && user.authenticate(params[:password])
+    @title = "Log in"
+    user = User.find_by_username(params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:success] = "Logged in!"
       redirect_to root_url
