7c3ba38cd68f217798636466ae14a4e6d7cda217
Ming-Ho Yee <ming-ho.yee@uwaterloo.ca>
2011-12-26 22:00:20 -0500

    Add basic authentication for production (Christmas season is now over)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate! if Rails.env.production?
+
   protect_from_forgery
   include SessionsHelper
 
@@ -6,6 +8,12 @@ private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate!
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ConfigSecrets[:username] && password == ConfigSecrets[:password]
+    end
   end
 
   helper_method :current_user
